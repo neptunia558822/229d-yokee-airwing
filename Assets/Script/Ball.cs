0e47818b4f2e8c8d1d6e1c73715f9f71aef64702
neptunia558822 <neptunia558822@gmail.com>
2024-03-19 23:00:09 +0700

    ทำลูกศรสำหรับโต๊ะได้เเล้ว

---
+++ Assets/Script/Ball.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BallColor
+{
+    White,
+    Red,
+    Yellow,
+    Green,
+    Brown,
+    Blue,
+    Pink,
+    Black
+}
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField]
+    private int point;
+    public int Point {  get { return point; } }
+
+    [SerializeField]
+    private BallColor ballColor;
+
+    [SerializeField]
+    private MeshRenderer rd;
+
+    public void SetColorAndPoint(BallColor col)
+    {
+        switch (col)
+        {
+            case BallColor.White:
+                point = 0;
+                rd.material.color = Color.white;
+                break;
+            case BallColor.Red:
+                point = 1;
+                rd.material.color = Color.red;
+                break;
+            case BallColor.Yellow:
+                point = 2;
+                rd.material.color = Color.yellow;
+                break;
+            case BallColor.Green:
+                point = 3;
+                rd.material.color = Color.green;
+                break;
+            case BallColor.Brown:
+                point = 4;
+                rd.material.color = new Color32 (255, 123, 0, 255);
+                break;
+            case BallColor.Blue:
+                point = 5;
+                rd.material.color = Color.blue;
+                break;
+            case BallColor.Pink:
+                point = 6;
+                rd.material.color = new Color32 (255, 105, 224, 255);
+                break;
+            case BallColor.Black:
+                point = 7;
+                rd.material.color = Color.black;
+                break;
+        }
+    }
+
+    void Awake()
+    {
+        rd = GetComponent<MeshRenderer>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private int playerScore;
+    public int PlayerScore { get { return playerScore; } set {  playerScore = value; } }
+
+    [SerializeField]
+    private GameObject ballPrefab;
+
+    [SerializeField]
+    private GameObject[] ballPosition;
+
+    public static GameManager instance;
+
+    [SerializeField]
+    private GameObject cueBall;
+
+    [SerializeField]
+    private float xInput;
+
+    private void SetBall(BallColor col, int i)
+    {
+        GameObject obj = Instantiate(ballPrefab ,
+                                     ballPosition[i].transform.position,
+                                     Quaternion.identity);
+
+        Ball b = obj.GetComponent<Ball>();
+        b.SetColorAndPoint(col);
+    }
+
+    private void RotateBall()
+    {
+        xInput = Input.GetAxis("Horizontal");
+        cueBall.transform.Rotate(new Vector3(0f, xInput, 0f));
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+
+        SetBall(BallColor.Red, 1);
+        SetBall(BallColor.Yellow, 2);
+        SetBall(BallColor.Green, 3);
+        SetBall(BallColor.Brown, 4);
+        SetBall(BallColor.Blue, 5);
+        SetBall(BallColor.Pink, 6);
+        SetBall(BallColor.Black, 7);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RotateBall();
+    }
+}
